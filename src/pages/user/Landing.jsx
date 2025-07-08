06f04950d9f068637cb54b0a229e5cb8a54bb503
mohammedshanid <123456789+odin@users.noreply.github.com>
2025-07-08 11:17:23 +0000

    get in touch section added and styled completed

--- src/pages/user/Landing.jsx
+++ src/pages/user/Landing.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import { images } from '../../assets/images'
+import {
+  Linkedin,
+  Facebook,
+  Instagram,
+  PhoneCall,
+  Mail,
+  MapPin
+} from 'lucide-react';
+
 
 export default function Landing() {
   return (
@@ -13,7 +22,7 @@ export default function Landing() {
             <a href="#">About</a>
           </div>
           <div className='text-2xl font-bold text-green-800'>
-          FitAura
+            FitAura
           </div>
 
           <div className='flex space-x-12 text-md font-medium text-gray-900'>
@@ -26,8 +35,8 @@ export default function Landing() {
 
       {/* HERO */}
       <section className="bg-orange-100 min-h-[430px] py-24 px-4 rounded-[50px] max-w-5xl  mx-auto my-12 h-100 relative overflow-hidden ">
-       
-        <img src={images.hero1} 
+
+        <img src={images.hero1}
           alt="Decorations"
           className="absolute w-[420px] bottom-[-80px] left-[-85px] z-0"
         />
@@ -80,7 +89,7 @@ export default function Landing() {
         <div className='text-center mb-14'>
           <p className='text-md uppercase tracking-wider text-gray-500 mb-4'>Service</p>
           <h1 className='text-3xl md:text-5xl font-bold text-green-900 mb-2'>
-            Your Path to <br  className=''/>Well-being
+            Your Path to <br className='' />Well-being
           </h1>
           <p className='text-gray-600 max-w-sm mx-auto text-xl mt-6'>
             Discover expert guidance for a healtheir mind and balanced life.
@@ -109,23 +118,23 @@ export default function Landing() {
           </div>
         </div>
       </section>
-      
+
       <section className='bg-gray-50 rounded-3xl shadow-xl p-6 md:p-10 flex flex-col md:flex-row items-center justify-between max-w-5xl mx-auto mt-10 mb-20'>
         <div className='md:w-1/2 text-center md:text-left'>
-        <h2 className='text-2xl md:text-4xl font-semibold text-gray-900 mb-4'>Wellness Coaching</h2>
-        <p className='text-gray-700 mb-2 font-semibold'>
-          Persanalized coaches support to help you build healthier habits,manage stress,and acheive balance in all area of your life.
-        </p>
-        <p className='text-gray-700 mb-6 font-semibold'>
-          Our wellness coaches support you in creating sustainable rountine for mental, emotional, and physical well-being.
-        </p>
-        <button className='bg-green-900 hover:bg-green-800 text-white font-medium px-5 py-2 rounded-full transition'>
-          Learn More
-        </button>
+          <h2 className='text-2xl md:text-4xl font-semibold text-gray-900 mb-4'>Wellness Coaching</h2>
+          <p className='text-gray-700 mb-2 font-semibold'>
+            Persanalized coaches support to help you build healthier habits,manage stress,and acheive balance in all area of your life.
+          </p>
+          <p className='text-gray-700 mb-6 font-semibold'>
+            Our wellness coaches support you in creating sustainable rountine for mental, emotional, and physical well-being.
+          </p>
+          <button className='bg-green-900 hover:bg-green-800 text-white font-medium px-5 py-2 rounded-full transition'>
+            Learn More
+          </button>
         </div>
 
         <div className='md:w-1/2 mt-8 md:mt-0 flex justify-center'>
-        <img src={images.modal1} alt="" className='max-w-xs w-full h-auto'/>
+          <img src={images.modal1} alt="" className='max-w-xs w-full h-auto' />
         </div>
       </section>
 
@@ -133,16 +142,16 @@ export default function Landing() {
         <div className='text-center mb-12 max-w-3xl mx-auto'>
           <p className='text-md tracking-wide uppercase text-gray-600 mb-2'>Explore & Learn</p>
           <h1 className='text-3xl md:text-4xl font-bold text-gray-900 mb-4'>
-             Resources for <br className="" /> Your Well-being
+            Resources for <br className="" /> Your Well-being
           </h1>
           <p className='text-gray-700 font-semibold text-xl'>
-            Explore expert insights, self-care guides,<br/> and tools to support your mental health.
+            Explore expert insights, self-care guides,<br /> and tools to support your mental health.
           </p>
         </div>
 
         <div className='grid grid-cols-1 md:grid-cols-3 gap-6 max-w-6xl mx-auto'>
           <div className='bg-white rounded-[50px] p-8 shadow-md text-center max-w-xs'>
-            <h2 className='text-2xl  font-semibold text-gray-900  mb-4'>Article & <br/> Guides</h2>
+            <h2 className='text-2xl  font-semibold text-gray-900  mb-4'>Article & <br /> Guides</h2>
             <p className='text-gray-600 mb-8 font-semibold'>
               Practical tips on stress management, mindfulness, and emotional resilience.
             </p>
@@ -151,7 +160,7 @@ export default function Landing() {
             </button>
           </div>
           <div className='bg-white rounded-[50px] p-8 shadow-md text-center max-w-xs'>
-            <h2 className='text-2xl  font-semibold text-gray-900  mb-4'>Routines & <br/> Guides</h2>
+            <h2 className='text-2xl  font-semibold text-gray-900  mb-4'>Routines & <br /> Guides</h2>
             <p className='text-gray-600 mb-8 font-semibold'>
               Practical tips on stress management, mindfulness, and emotional resilience.
             </p>
@@ -160,7 +169,7 @@ export default function Landing() {
             </button>
           </div>
           <div className='bg-white rounded-[50px] p-8 shadow-md text-center max-w-xs'>
-            <h2 className='text-2xl  font-semibold text-gray-900  mb-4'>Nutrition & <br/> Guides</h2>
+            <h2 className='text-2xl  font-semibold text-gray-900  mb-4'>Nutrition & <br /> Guides</h2>
             <p className='text-gray-600 mb-8 font-semibold'>
               Practical tips on stress management, mindfulness, and emotional resilience.
             </p>
@@ -172,73 +181,141 @@ export default function Landing() {
       </section>
 
       <section className='bg-gray-50 rounded-[50px] p-6  md:px-14 flex flex-col  md:flex-row item-center justify-between max-w-6xl mx-auto relative overflow-hidden'>
-        
+
         <div className='max-w-sm text-center md:text-left '>
-        <p className='text-xs uppercase tracking-widest text-gray-600 mt-4 mb-4'>Community</p>
-        <h2 className='text-3xl md:text-4xl font-bold text-gray-900 mb-6 leading-snug'>
-          You’re Not Alone <br className="" /> on This Journey
-        </h2>
-        <p className='text-gray-700 mb-10 font-semibold text-xl'>
-          Connect with others, share experiences, and find encouragement in a safe, supportive space.
-        </p>
-        <button className='bg-green-900 hover:bg-green-800 text-white font-medium px-5 py-2 rounded-full transition mb-8'>
-          Join The Community
-        </button>
+          <p className='text-xs uppercase tracking-widest text-gray-600 mt-4 mb-4'>Community</p>
+          <h2 className='text-3xl md:text-4xl font-bold text-gray-900 mb-6 leading-snug'>
+            You’re Not Alone <br className="" /> on This Journey
+          </h2>
+          <p className='text-gray-700 mb-10 font-semibold text-xl'>
+            Connect with others, share experiences, and find encouragement in a safe, supportive space.
+          </p>
+          <button className='bg-green-900 hover:bg-green-800 text-white font-medium px-5 py-2 rounded-full transition mb-8'>
+            Join The Community
+          </button>
         </div>
 
         <div className='absolute w-[700px] bottom-0 right-0 md:bottom-[-90px] md:right-[-90px] z-0'>
-        <img src={images.modal2} alt="" />
+          <img src={images.modal2} alt="" />
         </div>
-        
+
       </section>
 
       <section className='bg-gray-50  mt-3 py-20 px-6 md:px-12 max-w-6xl mx-auto relative'>
         <div className='flex flex-col md:flex-row items-center justify-between gap-1'>
           <div className=' text-center md:text-left'>
-          <p className='text-md uppercase tracking-widest text-gray-600 mb-6'>Need Help?</p>
-          <h2 className='text-3xl md:text-4xl font-bold text-gray-900 mb-6'>
-            Frequently <br className=""/> Asked <span class="underline underline-offset-4 decoration-green-700">Questions</span>
-          </h2>
-          <p className='text-gray-700 mb-8'>
-            Find answers to common questions about our services, therapy, and mental well-being.
-          </p>
-          <div className='bg-orange-100 px-6 py-[120px] mt-[250px] rounded-3xl  w-full max-w-md mx-auto md:mx-0'></div>
-        <img src={images.modal3} alt="FAQ Illustration" className='absolute w-[500px] bottom-[1px] left-[30px]' />
+            <p className='text-md uppercase tracking-widest text-gray-600 mb-6'>Need Help?</p>
+            <h2 className='text-3xl md:text-4xl font-bold text-gray-900 mb-6'>
+              Frequently <br className="" /> Asked <span class="underline underline-offset-4 decoration-green-700">Questions</span>
+            </h2>
+            <p className='text-gray-700 mb-8'>
+              Find answers to common questions about our services, therapy, and mental well-being.
+            </p>
+            <div className='bg-orange-100 px-6 py-[120px] mt-[250px] rounded-3xl  w-full max-w-md mx-auto md:mx-0'></div>
+            <img src={images.modal3} alt="FAQ Illustration" className='absolute w-[500px] bottom-[1px] left-[3no0px]' />
           </div>
           <div className='flex flex-col gap-4 mt-[250px]'>
-          <div className='bg-white rounded-xl p-4 flex items-center justify-between shadow-sm'>
-            <p className='text-gray-800 font-medium'>How do I book a therapy session?</p>
-            <span className='text-2xl text-gray-500 font-bold'>+</span>
-          </div>
-          <div className='bg-white rounded-xl p-4 flex items-center justify-between shadow-sm'>
-            <p className='text-gray-800 font-medium'>Are online sessions available?</p>
-            <span className='text-2xl text-gray-500 font-bold'>+</span>
-          </div>
-          <div className='bg-white rounded-xl p-4 flex items-center justify-between shadow-sm'>
-            <p className='text-gray-800 font-medium'>What’s the difference between therapy and coaching?</p>
-            <span className='text-2xl text-gray-500 font-bold'>+</span>
-          </div>
-          <div className='bg-white rounded-xl p-4 flex items-center justify-between shadow-sm'>
-            <p className='text-gray-800 font-medium'>Do I need a subscription to access services?</p>
-            <span className='text-2xl text-gray-500 font-bold'>+</span>
-          </div>
-          <div className='bg-white rounded-xl p-4 flex items-center justify-between shadow-sm'>
-            <p className='text-gray-800 font-medium'>Can I switch therapists if I don’t feel the right connection?</p>
-            <span className='text-2xl text-gray-500 font-bold'>+</span>
-          </div>
-          <div className='bg-white rounded-xl p-4 flex items-center justify-between shadow-sm'>
-            <p className='text-gray-800 font-medium'>Is my information and session history kept confidential?</p>
-            <span className='text-2xl text-gray-500 font-bold'>+</span>
-          </div>
-          
-          
-          
+            <div className='bg-white rounded-xl p-4 flex items-center justify-between shadow-sm'>
+              <p className='text-gray-800 font-medium'>How do I book a therapy session?</p>
+              <span className='text-2xl text-gray-500 font-bold'>+</span>
+            </div>
+            <div className='bg-white rounded-xl p-4 flex items-center justify-between shadow-sm'>
+              <p className='text-gray-800 font-medium'>Are online sessions available?</p>
+              <span className='text-2xl text-gray-500 font-bold'>+</span>
+            </div>
+            <div className='bg-white rounded-xl p-4 flex items-center justify-between shadow-sm'>
+              <p className='text-gray-800 font-medium'>What’s the difference between therapy and coaching?</p>
+              <span className='text-2xl text-gray-500 font-bold'>+</span>
+            </div>
+            <div className='bg-white rounded-xl p-4 flex items-center justify-between shadow-sm'>
+              <p className='text-gray-800 font-medium'>Do I need a subscription to access services?</p>
+              <span className='text-2xl text-gray-500 font-bold'>+</span>
+            </div>
+            <div className='bg-white rounded-xl p-4 flex items-center justify-between shadow-sm'>
+              <p className='text-gray-800 font-medium'>Can I switch therapists if I don’t feel the right connection?</p>
+              <span className='text-2xl text-gray-500 font-bold'>+</span>
+            </div>
+            <div className='bg-white rounded-xl p-4 flex items-center justify-between shadow-sm'>
+              <p className='text-gray-800 font-medium'>Is my information and session history kept confidential?</p>
+              <span className='text-2xl text-gray-500 font-bold'>+</span>
+            </div>
+
+
+
           </div>
         </div>
       </section>
-      
 
-      
+      <section className='bg-white py-20 px-4'>
+        <div className="text-white mb-14 text-center">
+          <p className='text-md uppercase tracking-widest text-gray-500 mb-3'>Get in touch</p>
+          <h2 className='text-3xl md:text-4xl font-bold text-green-900 mb-3'>
+            We’re Here to <br className="md:hidde" /> Support You
+          </h2>
+          <p className='text-gray-600 max-w-xl text-xl mx-auto'>
+            Whether you have questions, need help getting started, or want to learn more — reach out anytime.
+          </p>
+        </div>
+
+        <div className="bg-gray-50 rounded-3xl p-8 md:p-12 max-w-6xl mx-auto shadow grid md:grid-cols-2 gap-10">
+          <div className="">
+            <h3 className='text-2xl font-semibold text-green-900 mb-8' >Contact details</h3>
+            <p className='mb-3'>
+              <span className='font-bold text-gray-800'>Email: </span>
+              <span className='text-gray-700'>support@FitAure,com</span>
+            </p>
+            <p className='mb-3'>
+              <span className='font-bold text-gray-800'>Phone: </span>
+              <span className='text-gray-700'>+91 98435840395</span>
+            </p>
+            <p className='mb-3'>
+              <span className='font-bold text-gray-800'>Address: </span>
+              <span className='text-gray-700'>134 Wellness Way ,calm city, CA 534958t2</span>
+            </p>
+
+            <div className='flex items-center gap-4 text-green-900 mt-8'>
+              <Linkedin className="w-5 h-5 hover:text-green-700 cursor-pointer" />
+              <Facebook className="w-5 h-5 hover:text-green-700 cursor-pointer" />
+              <Instagram className="w-5 h-5 hover:text-green-700 cursor-pointer" />
+              <PhoneCall className="w-5 h-5 hover:text-green-700 cursor-pointer" />
+            </div>
+            <p className='text-md text-gray-500 mt-20'>We typically respond within 12 hours.</p>
+          </div>
+
+          <div>
+            <h3 className='text-2xl font-semibold text-green-900 mb-8'>Send Us a Message</h3>
+            <div className="bg-orange-50 p-10 rounded-[40px] shadow-inner">
+              <form action="" className='space--y'>
+                <div className='mb-3'>
+                  <label htmlFor="" className='block text-sm text-gray-800 mb-1'>Email</label>
+                  <input type="email" placeholder='Email' className='w-full border-b border-gray-400 bg-transparent focus:outline-none py-2' />
+                </div>
+                <div className='mb-8'>
+                  <label htmlFor="" className='block text-sm text-gray-800 mb-1'>Message</label>
+                  <textarea name="" placeholder='Message' className='w-full border-b border-gray-400 bg-transparent focus:outline-none py-2' id=""></textarea>
+                </div>
+                <button className='bg-green-900 text-white w-full py-2 rounded-full hover:bg-green-800 transition'>
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className='bg-gray-50 px-4 py-10'>
+        <div className='max-w-6xl mx-auto grid md:grid-cols-2 gap-8'>
+          <div className='bg-white rounded-[40px] p-6 shadow-sm'>
+            <h2 className='text-xl font-bold mb-4 text-green-900'>FitAura</h2>
+            <div className='grid grid-cols-3 gap-4 text'></div>
+          </div>
+        </div>
+      </footer>
+
+
+
+
+
     </div>
   );
 }
