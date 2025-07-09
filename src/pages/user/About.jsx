6476ef07f54253df6461bfd883958deec10db7aa
mohammedshanid <123456789+odin@users.noreply.github.com>
2025-07-09 06:12:46 +0000

    about section cantact reference added and styled

--- src/pages/user/About.jsx
+++ src/pages/user/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { images } from '../../assets/images'
+import { Mail, Phone, MapPin } from "lucide-react";
 
 export default function About(){
   return(
@@ -16,6 +17,34 @@ export default function About(){
         </div>
       </div>
     </section>
+
+    <section className="bg-black text-white py-20 px-4">
+      <div className="max-w-6xl mx-auto text-center">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4">Reach out to us today</h2>
+        <p className="text-gray-400 mb-12 max-w-xl mx-auto">
+          Whether you need support or want to learn more, we're here to assist you
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div className="bg-gray-900 rounded-xl p-6 flex flex-col items-center shadow hover:shadow-lg transition">
+            <Mail className="w-8 h-8 mb-4 text-white"/>
+            <h3 className="text-lg font-semibold mb-1">Email</h3>
+            <p className="text-md text-gray-400">FitAura@gmail.com</p>
+          </div>
+          <div className="bg-gray-900 rounded-xl p-6 flex flex-col items-center shadow hover:shadow-lg transition">
+            <Phone className="w-8 h-8 mb-4 text-white"/>
+            <h3 className="text-lg font-semibold mb-1">Phone</h3>
+            <p className="text-md text-gray-400">+91 09754584588</p>
+          </div>
+          <div className="bg-gray-900 rounded-xl p-6 flex flex-col items-center shadow hover:shadow-lg transition">
+            <MapPin className="w-8 h-8 mb-4 text-white"/>
+            <h3 className="text-lg font-semibold mb-1">Address</h3>
+            <p className="text-md text-gray-400">Kozhikode,kerala,india</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
     </>
   )
 }
