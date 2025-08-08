2d1e39698ea6b75013d2f3f4516f137b0667b9d5
mohammedshanid <123456789+odin@users.noreply.github.com>
2025-08-08 16:15:33 +0000

    base_url updated

--- client/src/services/base_url.js
+++ client/src/services/base_url.js
@@ -1,2 +1,4 @@
 // ✅ Correct
-export const base_url = "http://localhost:3000/api";
+// export const base_url = "http://localhost:3000/api";
+export const base_url = "https://fitaura-im72.onrender.com/api";
+
